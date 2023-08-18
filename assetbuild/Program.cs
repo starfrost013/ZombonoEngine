2c7740a907668667f912c6e6958efce76d4f92e6
starfrost013 <mario64crashed@gmail.com>
2023-08-18 23:45:41 +0100

    add fimg (for lmp edit, temporary until i write my own)
    replace console back as an example

---
+++ assetbuild/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Constants
+const string ASSETBUILD_VERSION = "1.0.0";
+#endregion
+
+#region Strings
+const string STRING_SIGNON = $"Assetbuild {ASSETBUILD_VERSION}";
+const string STRING_DESCRIPTION = $"Builds assets for Zombono";
+const string STRING_USAGE = $"Assetbuild <game>\n\n" +
+    $"<game>: Path to directory contaning game files";
+#endregion
+
+Console.WriteLine(STRING_SIGNON);
+Console.WriteLine(STRING_DESCRIPTION);
